64331e6266eb03800b9e355834ba1407cb182f98
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-05 09:53:55 -0600

    fix what I was trying to refactor too soon

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,19 +4,15 @@ class SearchController < ApplicationController
     end
 
     def find_members(nation)
-        response = conn.get("characters?affiliation=#{nation}&perPage=10000")
-        results = parse(response)
-
-        {population: results.count,
-        characters: results[0..24]}
-        binding.pry
+        response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{nation.gsub('_', '+')}&perPage=10000")
+        body = JSON.parse(response.body, symbolize_names: true)
     end
 
     def conn 
         @conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/api/v1")
     end
 
-    def parse(json)
-        JSON.parse(json.body, symbolize_names: true)
-    end
+    # def parse(json)
+    #     JSON.parse(json.body, symbolize_names: true)
+    # end
 end 
